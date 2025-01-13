313ba5bd555ebddc35095dfe600e2f0d088ca5d3
MikaelGille <mikael.gille@gmail.com>
2025-01-13 18:29:46 +0100

    Changed name for class

---
+++ ToDoAppWPF/TodoList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoAppWPF
+{
+    public class TodoList
+    {
+        private List<string> tasks = new List<string>();
+
+        public bool AddTask(string task)
+        {
+            if (string.IsNullOrWhiteSpace(task))
+            {
+                return false;
+            }
+            tasks.Add(task);
+            return true;
+        }
+
+        public bool RemoveTask(int index)
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+                tasks.RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+
+        public List<string> GetAllTasks()
+        {
+            return new List<string>(tasks);
+        }
+    }
+}
